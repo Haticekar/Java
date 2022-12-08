fd41450c88e22b4192325e7d7aa53a0b34eed957
Haticekar <107208951+Haticekar@users.noreply.github.com>
2022-12-08 18:16:36 +0300

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,26 @@
+package youtube;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		CreditManager creditManager=new CreditManager();
+		creditManager.add();
+		creditManager.calculate();
+		
+		MortgageManager mortgageManager=new MortgageManager();
+		mortgageManager.add();
+		
+		CreditManager[] credits=new CreditManager[2];
+		credits[0]=new MortgageManager();
+		credits[1]=new VehicleManager();
+		
+		for(int i=0;i<credits.length;i++) {
+			credits[i].calculate();
+		}
+				
+		
+
+	}
+// SOLID PRENSİPLERİ
+	
+}

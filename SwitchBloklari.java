0ab1e6f0cf6aba7c19b2824869f9571c6551dc58
Haticekar <107208951+Haticekar@users.noreply.github.com>
2022-12-08 20:48:08 +0300

    Add files via upload

---
+++ SwitchBloklari.java
@@ -0,0 +1,31 @@
+package youtube;
+
+public class SwitchBloklari {
+
+	public static void main(String[] args) {
+		char grade='a';
+		switch(grade) {
+		case 'A':
+			System.out.println("Mükemmel : Geçtiniz.");
+			break;
+			
+		case 'B':
+			System.out.println("Çok Güzel: Geçtiniz");
+			break;
+		case 'C':
+			System.out.println("Güzel: Geçtiniz");
+			break;
+		case 'D':
+			System.out.println("Fena Değil : Geçtiniz");
+			break;
+		case 'F':
+			System.out.println("Maalesef Kaldınız");
+			break;
+			default:
+				System.out.println("Geçersiz bir giriş yaptınız.");
+			
+		}
+
+	}
+
+}

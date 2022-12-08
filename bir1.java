b3a26db3be3f21c0e1777874c69519692d515e20
Haticekar <107208951+Haticekar@users.noreply.github.com>
2022-12-08 18:12:29 +0300

    Add files via upload

---
+++ bir1.java
@@ -0,0 +1,30 @@
+package youtube;
+
+import javax.print.attribute.DocAttribute;
+
+public class bir1 {
+
+	public static void main(String[] args) {
+		String urun="Laptop";
+		for(int i=1;i<11;i++) {
+			System.out.println(i+ " " + urun);
+			
+		}
+		
+		int sayi=10;
+		while(sayi<=100) {
+			System.out.println(sayi);
+			sayi+=10;
+			
+		}
+		int sayi1=20;
+		do { //DO WHILE DONGUSUNDE ŞART DO NUN ALTINDA OLDUĞU İÇİN MUTLAKA BİR TANE ÇIKTI YAZILIR.
+			System.out.println(sayi1);
+			sayi1+=20;
+		}while(sayi1>200);
+		
+	
+
+	}
+
+}
